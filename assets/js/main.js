bd21e49e340fcf5b64086023217d8feeb9316caf
Yann Bolliger <yann.bolliger@epfl.ch>
2018-12-15 16:52:24 +0100

    Add fade effect on scroll.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,33 @@
 
-$(() => {
+$(function()  {
 
-  setTimeout(() => $(".question").addClass("active"), 2000)
+  // debounce scroll handler
+  // handly scroll every
+  var SCROLL_INTERVAL = 400; // ms
 
-})
+  var hasScrolled = true;
+  $(window).scroll(function() {
+    hasScrolled = true;
+  })
+
+  setInterval(function() {
+    if (!hasScrolled) return;
+
+    revealQuestions();
+    hasScrolled = false;
+
+  }, SCROLL_INTERVAL);
+
+  function revealQuestions() {
+    var scrollPos = $(document).scrollTop();
+    var windowHeight = $(window).height();
+
+    $(".question").each(function(){
+      var questionPosition = $(this).offset().top;
+
+      if (questionPosition < scrollPos + windowHeight*0.33) {
+        $(this).addClass("active");
+      }
+    });
+  }
+});
